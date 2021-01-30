e7f2f4c2be4c9455b5f13ff7f5362d4b88349c08
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2021-01-31 00:00:19 +0700

    Constants

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+// ? colors
+final primaryCollor = Color.fromRGBO(26, 136, 50, 1);
+final secondaryCollor = Color.fromRGBO(255, 199, 0, 1);
+final tertiaryCollor = Color.fromRGBO(229, 229, 229, 1);
