81dedf93d9b5f7704c1264ba5cee3163c06bf936
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2021-01-31 22:42:38 +0700

    Menu Model

---
+++ lib/model/menu.dart
@@ -0,0 +1,9 @@
+class Menu {
+  final String name;
+  final String restaurant;
+  final String price;
+  final double rating;
+  final String picture;
+
+  Menu({this.name, this.restaurant, this.price, this.rating, this.picture});
+}
