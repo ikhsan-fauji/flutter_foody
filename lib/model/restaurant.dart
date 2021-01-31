7e567cd5b8b2b43b969c9c046a3999e17f3b3fdf
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2021-01-31 19:49:20 +0700

    restaurant model

---
+++ lib/model/restaurant.dart
@@ -0,0 +1,10 @@
+class Restaurant {
+  final String id;
+  final String name;
+  final String description;
+  final String pictureId;
+  final String city;
+  final double rating;
+
+  Restaurant({this.id, this.name, this.description, this.pictureId, this.city, this.rating});
+}
